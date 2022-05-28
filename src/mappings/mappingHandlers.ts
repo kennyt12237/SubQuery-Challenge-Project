4679447296d0c16086e1354e305126280350ee91
unknown <kennyt12237@gmail.com>
2022-05-28 13:34:32 +1200

    Implemented the handle event mapping function to create account and block

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -2,5 +2,34 @@ import {SubstrateEvent} from "@subql/types";
 import {Block, Account} from "../types";
 
 export async function handleEvent(event: SubstrateEvent): Promise<void> {
+// Extract block information from argument
+const blockID = event.block.block.header.hash.toString();
+const blockNumber = event.block.block.header.number;
+const timestamp = event.block.timestamp.toISOString();
 
+// Fetch block from database
+const block = await Block.get(blockID);
+
+// Create new block if not in database
+if (!block) {
+  const newBlock = new Block(blockID);
+  newBlock.timestamp = new Date(timestamp);
+  newBlock.blockNumber = blockNumber.toBigInt();
+  await newBlock.save();
+}
+
+// Extract event information from argument
+const eventIndex = event.idx;
+const address = event.event.data[0].toString();
+
+// Fetch account from database
+const account = await Account.getByAddress(address);
+
+// Create new account if not in database
+if (!account) {
+  const newAccount = new Account(`${blockNumber.toNumber()}-${eventIndex}`);
+  newAccount.address = address;
+  newAccount.blockId = blockID;
+  await newAccount.save();
+}
 }
